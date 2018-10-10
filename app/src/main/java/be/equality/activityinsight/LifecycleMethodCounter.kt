37340355810aaa2b52116f8bbe2b87962c07ac0b
Harm De Weirdt <harm.deweirdt@hogent.be>
2018-10-10 09:46:20 +0200

    Added default values to LifeCycleMethodCounter properties

--- app/src/main/java/be/equality/activityinsight/LifecycleMethodCounter.kt
+++ app/src/main/java/be/equality/activityinsight/LifecycleMethodCounter.kt
@@ -5,7 +5,10 @@ import java.io.Serializable
 /**
  * Data class which contains all the counters of the life cycle methods.
  */
-data class LifecycleMethodCounter(var onCreate : Int, var onStart : Int, var onResume : Int,
-                                  var onRestart : Int, var onPause: Int, var onStop: Int,
-                                  var onDestroy : Int) : Serializable {
-}
+data class LifecycleMethodCounter(var onCreate: Int = 0,
+                                  var onStart: Int = 0,
+                                  var onResume: Int = 0,
+                                  var onRestart: Int = 0,
+                                  var onPause: Int = 0,
+                                  var onStop: Int = 0,
+                                  var onDestroy: Int = 0) : Serializable
