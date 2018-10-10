143701d47a5dd66dc4672315c03902729afca37b
Harm De Weirdt <harm.deweirdt@hogent.be>
2018-10-10 09:58:32 +0200

    Reworked LifeCycleMethodCounter to better enforce encapsulation
    Now it only exposes methods that increment the relevant counter.
    This disallows users of the class to directly set the values to another value

--- app/src/main/java/be/equality/activityinsight/LifecycleMethodCounter.kt
+++ app/src/main/java/be/equality/activityinsight/LifecycleMethodCounter.kt
@@ -4,11 +4,56 @@ import java.io.Serializable
 
 /**
  * Data class which contains all the counters of the life cycle methods.
+ *
+ * Attributes are private so they can only be incremented, not changed directly
  */
-data class LifecycleMethodCounter(var onCreate: Int = 0,
-                                  var onStart: Int = 0,
-                                  var onResume: Int = 0,
-                                  var onRestart: Int = 0,
-                                  var onPause: Int = 0,
-                                  var onStop: Int = 0,
-                                  var onDestroy: Int = 0) : Serializable
+class LifecycleMethodCounter(onCreate: Int = 0,
+                             onStart: Int = 0,
+                             onResume: Int = 0,
+                             onRestart: Int = 0,
+                             onPause: Int = 0,
+                             onStop: Int = 0,
+                             onDestroy: Int = 0) : Serializable {
+    var onCreate: Int = onCreate
+        private set
+    var onStart: Int = onStart
+        private set
+    var onResume: Int = onResume
+        private set
+    var onRestart: Int = onRestart
+        private set
+    var onPause: Int = onPause
+        private set
+    var onStop: Int = onStop
+        private set
+    var onDestroy: Int = onDestroy
+        private set
+
+    fun onDestroyCalled() {
+        onDestroy++
+    }
+
+    fun onStopCalled() {
+        onStop++
+    }
+
+    fun onPauseCalled() {
+        onPause++
+    }
+
+    fun onRestartCalled() {
+        onRestart++
+    }
+
+    fun onResumeCalled() {
+        onResume++
+    }
+
+    fun onStartCalled() {
+        onStart++
+    }
+
+    fun onCreateCalled() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
--- app/src/main/java/be/equality/activityinsight/MainActivity.kt
+++ app/src/main/java/be/equality/activityinsight/MainActivity.kt
@@ -1,56 +1,56 @@
 package be.equality.activityinsight
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
-    private var counter : LifecycleMethodCounter = LifecycleMethodCounter()
+    private var counter: LifecycleMethodCounter = LifecycleMethodCounter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        counter.onCreate++
+        counter.onCreateCalled()
         textViewOnCreateNumber.text = "${counter.onCreate}"
     }
 
     override fun onStart() {
         super.onStart()
-        counter.onStart++;
+        counter.onStartCalled()
         textViewOnStartNumber.text = "${counter.onStart}"
 
     }
 
     override fun onResume() {
         super.onResume()
-        counter.onResume++
+        counter.onResumeCalled()
         textViewOnResumeNumber.text = "${counter.onResume}"
     }
 
     override fun onRestart() {
         super.onRestart()
-        counter.onRestart++
+        counter.onRestartCalled()
         textViewOnRestartNumber.text = "${counter.onRestart}"
     }
 
     override fun onPause() {
         super.onPause()
-        counter.onPause++
-        textViewOnPausetNumber.text =  "${counter.onPause}"
+        counter.onPauseCalled()
+        textViewOnPausetNumber.text = "${counter.onPause}"
 
     }
 
-    override fun onStop(){
+    override fun onStop() {
         super.onStop()
-        counter.onStop++
+        counter.onStopCalled()
         textViewOnStoptNumber.text = "${counter.onStop}"
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        counter.onDestroy++
+        counter.onDestroyCalled()
         textViewOnDestroyNumber.text = "${counter.onDestroy}"
     }
 
